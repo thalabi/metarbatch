cf66335079d97c2f01b59e1f4b1cbcbfc134035a
Tarif Halabi <tarif.halabi@gmail.com>
2022-03-27 23:20:11 -0400

    Add jdbcTemplate beans for respective datasources.

--- metarbatch/src/main/java/com/kerneldc/metarbatch/config/DatabaseConfig.java
+++ metarbatch/src/main/java/com/kerneldc/metarbatch/config/DatabaseConfig.java
@@ -2,15 +2,17 @@ package com.kerneldc.metarbatch.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.batch.BatchDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
-public class DataSourceConfig {
+public class DatabaseConfig {
 
 	@Bean
 	@Primary
@@ -25,4 +27,16 @@ public class DataSourceConfig {
 	public DataSource batchDataSource() {
 		return DataSourceBuilder.create().build();
 	}
+	
+	@Bean
+	@Primary
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+	@Bean
+	@Qualifier("batchJdbcTemplate")
+    public JdbcTemplate batchJdbcTemplate(@Qualifier("batchDataSource") DataSource batchDataSource) {
+        return new JdbcTemplate(batchDataSource);
+    }
 }
