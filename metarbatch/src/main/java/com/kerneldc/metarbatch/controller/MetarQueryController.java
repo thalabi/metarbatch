16dcb1ed0c5f3d6e558cc4e786177aa0e539719d
Tarif Halabi <tarif.halabi@gmail.com>
2025-07-05 13:04:37 -0400

    Add metar query be date range.

--- metarbatch/src/main/java/com/kerneldc/metarbatch/controller/MetarQueryController.java
+++ metarbatch/src/main/java/com/kerneldc/metarbatch/controller/MetarQueryController.java
@@ -1,8 +1,10 @@
 package com.kerneldc.metarbatch.controller;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import com.kerneldc.metarbatch.domain.Metar;
 import com.kerneldc.metarbatch.repository.MetarRepository;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,4 +38,17 @@ public class MetarQueryController {
     	LOGGER.info(AppConstants.LOG_END);
     	return ResponseEntity.ok(metarList);
 	}
+
+	@GetMapping("/getMetarListInObervationTimeRange")
+	public ResponseEntity<List<Metar>> getMetarListInObervationTimeRange(@RequestParam @NotBlank String stationIds,
+			@RequestParam @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime fromObservationTime,
+			@RequestParam @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime toObservationTime) {
+    	LOGGER.info(AppConstants.LOG_BEGIN);
+		LOGGER.info("stationIds: [{}], fromObservationTime: [{}], toObservationTime: [{}]", stationIds, fromObservationTime, toObservationTime);
+		var stationIdList = Arrays.asList(stationIds.toUpperCase().split(","));
+		var metarList = metarRepository.findBetweenObservationTimes(stationIdList, fromObservationTime, toObservationTime);
+    	LOGGER.info(AppConstants.LOG_END);
+    	return ResponseEntity.ok(metarList);
+	}
+
 }
--- metarbatch/src/main/java/com/kerneldc/metarbatch/repository/MetarRepository.java
+++ metarbatch/src/main/java/com/kerneldc/metarbatch/repository/MetarRepository.java
@@ -1,5 +1,6 @@
 package com.kerneldc.metarbatch.repository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,8 +12,6 @@ import com.kerneldc.metarbatch.domain.MetarPk;
 
 public interface MetarRepository extends JpaRepository<Metar, MetarPk> {
 
-	//List<Metar> findLatestNoOfObservations2(Collection<String> stationIdCollection, int noOfObservations);
-	
 	@Query(value = """
 		with ranked_metars as (
 			select *, row_number() over (partition by station_id order by observation_time desc) as rn
@@ -70,4 +69,58 @@ public interface MetarRepository extends JpaRepository<Metar, MetarPk> {
 		order by station_id, observation_time desc
 		""", nativeQuery = true)
 	List<Metar> findLatestNoOfObservations(Collection<String> stationIdCollection, int noOfObservations);
+
+	@Query(value = """
+			select
+				raw_text,
+				station_id,
+				observation_time,
+				latitude,
+				longitude,
+			  temp_c,
+			  dewpoint_c,
+			  wind_dir_degrees,
+			  wind_speed_kt,
+			  wind_gust_kt,
+			  visibility_statute_mi,
+			  altim_in_hg,
+			  sea_level_pressure_mb,
+			  corrected,
+			  auto,
+			  auto_station,
+			  maintenance_indicator_on,
+			  no_signal,
+			  lightning_sensor_off,
+			  freezing_rain_sensor_off,
+			  present_weather_sensor_off,
+			  wx_string,
+				sky_cover_1,
+				cloud_base_ft_agl_1,
+				sky_cover_2,
+				cloud_base_ft_agl_2,
+				sky_cover_3,
+				cloud_base_ft_agl_3,
+				sky_cover_4,
+				cloud_base_ft_agl_4,
+				flight_category,
+				three_hr_pressure_tendency_mb,
+				maxt_c,
+				mint_c,
+				maxt24hr_c,
+				mint24hr_c,
+				precip_in,
+				pcp3hr_in,
+				pcp6hr_in,
+				pcp24hr_in,
+				snow_in,
+				vert_vis_ft,
+				metar_type,
+				elevation_m
+			 from metar
+	     where station_id in (:stationIdCollection)
+	       and observation_time between :fromObservationTime and :toObservationTime
+			order by station_id, observation_time
+			""", nativeQuery = true)
+		List<Metar> findBetweenObservationTimes(Collection<String> stationIdCollection, LocalDateTime fromObservationTime, LocalDateTime toObservationTime);
+
 }
