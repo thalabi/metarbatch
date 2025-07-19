ff74b00d291768bc3ab6a0409941da90f53d99e9
Tarif Halabi <tarif.halabi@gmail.com>
2025-07-19 14:06:23 -0400

    Switch metarStageWriter to using jdbc instead of jpa for improved performance.

--- metarbatch/src/main/java/com/kerneldc/metarbatch/config/BatchConfig.java
+++ metarbatch/src/main/java/com/kerneldc/metarbatch/config/BatchConfig.java
@@ -14,13 +14,14 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -41,7 +42,6 @@ import com.kerneldc.metarbatch.service.EmailService;
 import com.kerneldc.metarbatch.service.MetarService;
 import com.kerneldc.metarbatch.xml.schema.metar.METAR;
 
-import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -53,11 +53,11 @@ public class BatchConfig {
 	private final JobExplorer jobExplorer;
 	private final JobRepository jobRepository;
 	private final JdbcTemplate jdbcTemplate;
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	private final EmailService emailService;
 	private final MetarService metarService;
 	private final PlatformTransactionManager transactionManage;
-	private final MetarStageRepository metarStageRepository;
-	private final EntityManagerFactory entityManagerFactory;
+	private final MetarStageRepository metarStageRepository;	
 	
 	public static final String METAR_JOB = "MetarJob";
 
@@ -103,10 +103,48 @@ public class BatchConfig {
 	public ItemProcessor<METAR, MetarStage> metarProcessor() {
 		return new MetarProcessor();
 	}
+
 	@Bean
-	public JpaItemWriter<MetarStage> metarStageWriter() {
-		return new JpaItemWriterBuilder<MetarStage>().entityManagerFactory(entityManagerFactory).build();
+	public JdbcBatchItemWriter<MetarStage> metarStageWriter() {
+	    //NamedParameterJdbcTemplate namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(batchDataSource);
+
+	    JdbcBatchItemWriter<MetarStage> writer = new JdbcBatchItemWriter<>();
+	    writer.setJdbcTemplate(namedParameterJdbcTemplate);
+	    writer.setSql("""
+	        INSERT INTO metar_stage (
+	            raw_text, station_id, observation_time, latitude, longitude,
+	            temp_c, dewpoint_c, wind_dir_degrees, wind_speed_kt,
+	            wind_gust_kt, visibility_statute_mi, altim_in_hg,
+	            sea_level_pressure_mb, corrected, auto, auto_station,
+	            maintenance_indicator_on, no_signal, lightning_sensor_off,
+	            freezing_rain_sensor_off, present_weather_sensor_off,
+	            wx_string, sky_cover_1, cloud_base_ft_agl_1, sky_cover_2,
+	            cloud_base_ft_agl_2, sky_cover_3, cloud_base_ft_agl_3,
+	            sky_cover_4, cloud_base_ft_agl_4, flight_category,
+	            three_hr_pressure_tendency_mb, maxt_c, mint_c, maxt24hr_c,
+	            mint24hr_c, precip_in, pcp3hr_in, pcp6hr_in, pcp24hr_in,
+	            snow_in, vert_vis_ft, metar_type, elevation_m
+	        )
+	        VALUES (
+	            :rawText, :metarPk.stationId, :metarPk.observationTime, :latitude, :longitude,
+	            :tempC, :dewpointC, :windDirDegrees, :windSpeedKt,
+	            :windGustKt, :visibilityStatuteMi, :altimInHg,
+	            :seaLevelPressureMb, :corrected, :auto, :autoStation,
+	            :maintenanceIndicatorOn, :noSignal, :lightningSensorOff,
+	            :freezingRainSensorOff, :presentWeatherSensorOff,
+	            :wxString, :skyCover1, :cloudBaseFtAgl1, :skyCover2,
+	            :cloudBaseFtAgl2, :skyCover3, :cloudBaseFtAgl3,
+	            :skyCover4, :cloudBaseFtAgl4, :flightCategory,
+	            :threeHrPressureTendencyMb, :maxTC, :minTC, :maxT24HrC,
+	            :minT24HrC, :precipIn, :pcp3HrIn, :pcp6HrIn, :pcp24HrIn,
+	            :snowIn, :vertVisFt, :metarType, :elevationM
+	        )
+	    """);
+	    writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+	    writer.afterPropertiesSet();
+	    return writer;
 	}
+
 	@Bean
 	public Step insertMetarStageStep(ItemReader<METAR> metarListItemReader) {
 		return new StepBuilder("InsertMetarStage", jobRepository)
--- metarbatch/src/main/java/com/kerneldc/metarbatch/controller/MetarJobLauncherController.java
+++ metarbatch/src/main/java/com/kerneldc/metarbatch/controller/MetarJobLauncherController.java
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("metarJobLauncherController")
+@RequestMapping("/protected/metarJobLauncherController")
 @RequiredArgsConstructor
 @Slf4j
 public class MetarJobLauncherController {
