8b37f7e6c65e64e265bdfbb83c53147961dea8ed
Tarif Halabi <tarif.halabi@gmail.com>
2025-07-26 15:05:20 -0400

    Enable Actuator url requests logging.

--- metarbatch/src/main/java/com/kerneldc/metarbatch/config/WebLoggingConfig.java
+++ metarbatch/src/main/java/com/kerneldc/metarbatch/config/WebLoggingConfig.java
@@ -1,20 +1,47 @@
 package com.kerneldc.metarbatch.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class WebLoggingConfig {
 
+	// To enable, set
+	// logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter=debug
+	
 	@Bean
-    public CommonsRequestLoggingFilter requestLoggingFilter() {
-        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    public FilterRegistrationBean<CommonsRequestLoggingFilter>  requestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter() {
+        	@Override
+            protected boolean shouldLog(HttpServletRequest request) {
+                String path = request.getRequestURI();
+                // Log only requests to /actuator
+                return path.startsWith("/actuator");
+            }
+        	@Override
+        	protected void beforeRequest(HttpServletRequest request, String message) {
+        	    LOGGER.debug("Actuator request: [{}]", message);
+        	}
+        	// Only log before request urls
+        	@Override
+        	protected void afterRequest(HttpServletRequest request, String message) {
+        	    // do nothing — suppress after-request logging
+        	}
+        };
         filter.setIncludeClientInfo(true);
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
         filter.setIncludeHeaders(false); // change to true if needed
         filter.setMaxPayloadLength(10000);
-        return filter;
+        
+        FilterRegistrationBean<CommonsRequestLoggingFilter> registrationBean = new FilterRegistrationBean<>(filter);
+        registrationBean.setOrder(1); // Set order if you have multiple filters
+        return registrationBean;
     }
 }
